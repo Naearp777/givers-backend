4cb9b299bec820a5c1b7213adcdb14ee5b78d1fc
Saugat <saugatkafley@gmail.com>
2021-07-02 22:54:36 +0545

    Email with Html loaded on it .

--- givers/authentication/urls.py
+++ givers/authentication/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('api/users/login/',views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/users/profile/',views.getUserProfile,name='user-profile'),
@@ -18,9 +18,7 @@ urlpatterns = [
     path('api/password_reset/',include('django_rest_passwordreset.urls'),name='reset_password'),
 ]
 
-
-'''
-urlpatterns = [
-    url('^', include('django.contrib.auth.urls')),
+urlpatterns += [
+    path('template/home', TemplateView.as_view(template_name="home.html"), name='home'),
+    path('send_mail/', views.my_mail, )
 ]
-'''
--- givers/authentication/views.py
+++ givers/authentication/views.py
@@ -16,6 +16,11 @@ from rest_framework import generics
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.core.mail import send_mail, send_mass_mail
+
 
 # Create your views here.
 '''
@@ -118,3 +123,88 @@ class ChangePasswordView(generics.UpdateAPIView):
             return Response(response)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SendFormEmail(View):
+    
+    def  get(self, request):
+
+        # Get the form data 
+        name = request.GET.get('name', None)
+        email = request.GET.get('email', None)
+        message = request.GET.get('message', None)
+
+        # Send Email
+        send_mail(
+            'Subject - Django Email Testing', 
+            'Hello ' + name + ',\n' + message, 
+            'sender@example.com', # Admin
+            [
+                email,
+            ]
+        ) 
+
+        # Redirect to same page after form submit
+        messages.success(request, ('Email sent successfully.'))
+        return redirect('home') 
+
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives
+from django.http import HttpResponse  
+from django.conf import settings
+import os      
+from django.template.loader import render_to_string, get_template
+def my_mail(request):  
+    subject = "Greetings from Programink"  
+    msg_html = render_to_string('email_send.html')
+    
+    with open(os.path.join(settings.BASE_DIR, 'templates\email_send.html')) as f:
+        message = f.read()
+    
+    msg     = "template/email_send.html"  
+    to      = "saugatkafley@gmail.com"  
+    res     = EmailMultiAlternatives(subject,msg_html,settings.EMAIL_HOST_USER,[to])  
+    res.content_subtype = "html"  # Main content is now text/html
+    res.send()
+    if(res == 1):  
+        msg = "Mail Sent Successfully."  
+    else:  
+        msg = "Mail Sending Failed."  
+    return HttpResponse(msg)  
+
+'''
+from django.conf import settings
+
+def send_html_email(to_list, subject, templates, context, sender=settings.DEFAULT_FROM_EMAIL):
+    msg = EmailMessage(subject=subject, body=msg_html, from_email=sender)
+    return msg.send()
+
+
+def notify_subscribers(request):
+    # emails = Subscriber.objects.all().values_list('email', flat=True)
+    emails = 'saugatkafley@gmail.com'
+    context = {
+        'news': 'We have good news!'
+    }
+    
+    res = send_html_email("saugatkafley@gmail.com", 'Good news', 'email_send.html', context, "volunteermanagementsoftware@gmail.com")
+    if (res ==1):
+        return HttpResponse("Message sent")
+    else:
+        return HttpResponse("Message  not sent")
+
+import os
+def send_mail(request):
+    subject = "Email Message."
+    from_email = settings.EMAIL_HOST_USER
+    to_email =["saugatkafley@gmail.com"]
+    # os.path.join(settings.BASE_DIR, 'templates/email_send')
+    with open(os.path.join(settings.BASE_DIR, 'templates\email_send.txt')) as f:
+        message = f.read()
+    # with open(settings.BASE_DIR + os.path.join(settings.BASE_DIR,'templates/email_send.txt') )as f:
+    #     message = f.read()
+    mes = EmailMultiAlternatives(subject= subject, body =message, from_email = from_email, to_email = [to_email])
+    html_template = get_template("tempaltes/email_send.html").render()
+    mes.attach_alternative(html_template,"text/html")
+    mes.send()
+
+'''
