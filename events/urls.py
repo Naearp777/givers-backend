e7b894a7b6b172c35972f57fd12b097a98cc54b5
Arpan Pokharel <arpanpokharel73@gmail.com>
2021-07-17 18:53:24 +0545

    URLS customized

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns=[
 
-    path('api/events/',views.Event_display_all,name='all_events'),
+    path('api/events_ascending/',views.Event_display_all,name='all_events'),
     path('api/events/<int:E_id>/',views.Event_display_id,name='events_id'),
     path('api/events/user/<str:username>/',views.Event_display_specific,name='events_specific'),
     path('api/events/register/',views.registerEvent,name='register_events'),
--- miscellaneous/urls.py
+++ miscellaneous/urls.py
@@ -3,8 +3,9 @@ from .import views
 
 urlpatterns=[
 
-    path('api/events/sort/<int:category_id>/',views.show_specific_category,name='interested'),
+    path('api/events/sort/<int:category_id>/',views.show_specific_category,name='sort_category'),
     path('api/events/sort_desc/',views.show_event_desc,name='show_event_desc'),
+    path('api/events/',views.show_event_postedtime,name='show_posted_latest'),
     path('api/events/search/',views.searchevents,name='search_event'),
     path('api/users/search/',views.searchuser,name='search_user'),
 ]
