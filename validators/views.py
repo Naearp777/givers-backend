3c4df46ff72a4090235d751a4007e0bf6d7738f5
Arpan Pokharel <arpanpokharel73@gmail.com>
2021-07-21 20:04:21 +0545

    validators views updated

--- validators/views.py
+++ validators/views.py
@@ -4,30 +4,31 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 # Create your views here.
-@api_view(['GET'])
-def validateusername(request,username):
-    flag=0
-    if(User.objects.filter(username=username)):
-        
-        data={"message":"Username already exists"}
-        flag=1
-        return Response(data,status=status.HTTP_200_OK)
+@api_view(["GET"])
+def validateusername(request, username):
+    print(request)
+    flag = 0
+    if User.objects.filter(username=username):
 
-    elif flag==0:
-        data={"message":"Available"}
-        return Response(data,status=status.HTTP_200_OK)
+        data = {"message": "Username already exists", "available": False}
+        flag = 1
+        return Response(data, status=status.HTTP_200_OK)
+
+    elif flag == 0:
+        data = {"message": "Available", "available": True}
+        return Response(data, status=status.HTTP_200_OK)
 
 
-@api_view(['GET'])
-def validateemail(request,email):
-    flag =0
-    
-    if(User.objects.filter(email=email)):
+@api_view(["GET"])
+def validateemail(request, email):
+    flag = 0
 
-        data={"message":"Email already exists"}
-        flag=1
-        return Response(data,status=status.HTTP_200_OK)
+    if User.objects.filter(email=email):
 
-    elif flag==0:
-        data={"message":"Available"}
-        return Response(data,status=status.HTTP_200_OK)
+        data = {"message": "Email already exists", "available": False}
+        flag = 1
+        return Response(data, status=status.HTTP_200_OK)
+
+    elif flag == 0:
+        data = {"message": "Available", "available": True}
+        return Response(data, status=status.HTTP_200_OK)
