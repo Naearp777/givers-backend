6920805e742141b3b4e31a5833fdc190622fa8d1
Arpan Pokharel <arpanpokharel73@gmail.com>
2021-08-08 10:41:06 +0545

    host password added

--- givers/settings.py
+++ givers/settings.py
@@ -209,7 +209,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'volunteermanagementsoftware@gmail.com'
-EMAIL_HOST_PASSWORD = '!volunteer!'
+EMAIL_HOST_PASSWORD = 'dtxpfeyluqxvdujw'
+DEFAULT_FROM_EMAIL = 'Givers Team <noreply@volunteermanagementsystem.com>'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
