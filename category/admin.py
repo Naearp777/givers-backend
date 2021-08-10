4dd54974623269b63ecd4f6d2fdcbf7d55fd0bfb
Arpan Pokharel <arpanpokharel73@gmail.com>
2021-08-10 20:23:24 +0545

    Categories field added

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import EventCategory
+from .models import EventCategory, Skills, Country, Province, District, Municipality, Ward
 # Register your models here.
 admin.site.register(EventCategory)
+admin.site.register(Skills)
+admin.site.register(Country)
+admin.site.register(Province)
+admin.site.register(District)
+admin.site.register(Municipality)
+admin.site.register(Ward)
--- category/models.py
+++ category/models.py
@@ -8,3 +8,45 @@ class EventCategory(models.Model):
 
     def __str__(self):
         return f'{self.category}'
+
+
+class Country(models.Model):
+    country = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.country}'
+
+
+class Province(models.Model):
+    province = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.province}'
+
+
+class District(models.Model):
+    district = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.district}'
+
+
+class Municipality(models.Model):
+    Municipality = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.Municipality}'
+
+
+class Ward(models.Model):
+    ward = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.ward}'
+
+
+class Skills(models.Model):
+    skills = models.CharField(null=True, unique=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.skills}'
