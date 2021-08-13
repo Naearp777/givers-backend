330a0244347b351937655abe92f96a56d4404139
Arpan Pokharel <arpanpokharel73@gmail.com>
2021-08-13 12:32:30 +0545

    django adminstration changed to Givers

--- givers/urls.py
+++ givers/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+admin.site.site_header = "Givers"
+admin.site.site_title = "Givers"
+admin.site.index_title = "Welcome to Givers"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
