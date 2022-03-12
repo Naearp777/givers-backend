af80e8786d2442d0ec50ae80fc9804252f66069b
Arpan Pokharel <arpanpokharel73@gmail.com>
2022-03-12 14:09:07 +0545

    task assigned

--- volunteer/views.py
+++ volunteer/views.py
@@ -1,4 +1,4 @@
-
+from logging import exception
 from .models import interestedevents, requestevents
 from .serializers import interestedSerializervolunteer, requesteventSerializervolunteer
 from rest_framework import status
@@ -7,14 +7,15 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from customuser.models import User
 from events.models import Events
+
 # Create your views here.
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def requestevent(request, V_id, E_id):
     if requestevents.objects.filter(user_id=V_id, event_id=E_id).exists():
-        message = {'message': 'You have already requested to this event'}
+        message = {"message": "You have already requested to this event"}
         return Response(message)
     else:
         data = request.data
@@ -23,68 +24,64 @@ def requestevent(request, V_id, E_id):
                 user=User.objects.get(id=V_id),
                 event=Events.objects.get(id=E_id),
                 # description=data['description'],
-                ques_1=data['ques_1'],
-                ques_2=data['ques_2'],
-                ques_3=data['ques_3'],
-                ans_1=data['ans_1'],
-                ans_2=data['ans_2'],
-                ans_3=data['ans_3'],
-                request_volunteer=data['request_volunteer'],
-                approved=data['approved'],
-                pending=data['pending'],
-                file_1=data['file_1'],
-                task_assigned=data['task_assigned'],
+                ques_1=data["ques_1"],
+                ques_2=data["ques_2"],
+                ques_3=data["ques_3"],
+                ans_1=data["ans_1"],
+                ans_2=data["ans_2"],
+                ans_3=data["ans_3"],
+                request_volunteer=data["request_volunteer"],
+                approved=data["approved"],
+                pending=data["pending"],
+                file_1=data["file_1"],
+                task_assigned=data["task_assigned"],
             )
-            serializer = requesteventSerializervolunteer(
-                requestevent, many=False)
-            requestevent = requestevents.objects.get(id=serializer.data['id'])
-            requestevent.user_details = request.FILES.get('user_details')
+            serializer = requesteventSerializervolunteer(requestevent, many=False)
+            requestevent = requestevents.objects.get(id=serializer.data["id"])
+            requestevent.user_details = request.FILES.get("user_details")
             requestevent.save()
-            serializer = requesteventSerializervolunteer(
-                requestevent, many=False)
+            serializer = requesteventSerializervolunteer(requestevent, many=False)
 
             return Response(serializer.data)
-        except:
-            message = {
-                'detail': 'requestevents with this content already exists'}
-            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        except exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def showinterested(request, V_id):
     try:
-        interested = interestedevents.objects.filter(
-            user_id=V_id, interested=True)
+        interested = interestedevents.objects.filter(user_id=V_id, interested=True)
         serializer = interestedSerializervolunteer(interested, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except requestevents.DoesNotExist:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def interestedevent(request):
     data = request.data
     try:
         interested = interestedevents.objects.create(
-            user=User.objects.get(username=data['username']),
-            event=Events.objects.get(name=data['name']),
-            interested=data['interested'],
+            user=User.objects.get(username=data["username"]),
+            event=Events.objects.get(name=data["name"]),
+            interested=data["interested"],
         )
         serializer = requesteventSerializervolunteer(interested, many=False)
         return Response(serializer.data)
     except:
-        message = {'detail': 'You are already interested in this Event'}
+        message = {"detail": "You are already interested in this Event"}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def show_requested_specific_user(request, V_id):
     try:
         requested = interestedevents.objects.filter(
-            user_id=V_id, request_volunteer=True)
+            user_id=V_id, request_volunteer=True
+        )
         serializer = interestedSerializervolunteer(requested, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except requestevents.DoesNotExist:
